a41d73800c922f58bd8f18ad7573d56e9f888d73
divyanibhusari <divyanibhusari79@gmail.com>
2026-03-11 12:11:02 +0530

    this is a code

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -4,21 +4,41 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// passport.use(
+//     new GoogleStrategy(
+//         {
+//             clientID: process.env.GOOGLE_CLIENT_ID,
+//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//             callbackURL: "http://127.0.0.1:8000/api/admin/google/callback",
+
+//         },
+//         async (accessToken, refreshToken, profile, done) => {
+//             try {
+//                 // TODO: Save user in DB later
+//                 return done(null, profile);
+//             } catch (err) {
+//                 return done(err, null);
+//             }
+//         }
+//     )
+// );
+
 passport.use(
     new GoogleStrategy(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://127.0.0.1:8000/api/admin/google/callback",
-
+            callbackURL: "/api/admin/google/callback"
         },
         async (accessToken, refreshToken, profile, done) => {
-            try {
-                // TODO: Save user in DB later
-                return done(null, profile);
-            } catch (err) {
-                return done(err, null);
-            }
+
+            const user = {
+                displayName: profile.displayName,
+                email: profile.emails[0].value,
+                photo: profile.photos[0].value
+            };
+
+            return done(null, user);
         }
     )
 );
