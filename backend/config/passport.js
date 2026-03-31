d083fcb3a1d2f7577dc5483222c00d4c37b37a53
divyanibhusari <divyanibhusari79@gmail.com>
2026-03-31 12:22:48 +0530

    code

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -1,31 +1,3 @@
-// import passport from "passport";
-// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
-
-// passport.use(
-//     new GoogleStrategy(
-//         {
-//             clientID: process.env.GOOGLE_CLIENT_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//             callbackURL: "https://scrollfuel.in/api/admin/google/callback",
-//         },
-//         async (accessToken, refreshToken, profile, done) => {
-//             const user = {
-//                 displayName: profile.displayName,
-//                 email: profile.emails[0].value,
-//                 photo: profile.photos[0].value,
-//             };
-
-//             return done(null, user);
-//         }
-//     )
-// );
-
-// passport.serializeUser((user, done) => done(null, user));
-// passport.deserializeUser((user, done) => done(null, user));
-
-// export default passport;
-
-
 import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 
--- backend/routes/admin.router.js
+++ backend/routes/admin.router.js
@@ -1,30 +1,3 @@
-// // login,view,upload
-// import express from "express";
-// import { googleSuccess, Login, Signup } from "../controllers/controller.js";
-// import passport from "passport";
-
-// let router = express.Router();
-// router.post("/signup", Signup)
-// router.post("/login", Login)
-// // Start Google Login
-// router.get(
-//     "/google",
-//     passport.authenticate("google", {
-//         scope: ["profile", "email"],
-//     })
-// );
-
-// // Callback
-// router.get(
-//     "/google/callback",
-//     passport.authenticate("google", {
-//         failureRedirect: "http://localhost:5173/login",
-//     }),
-//     googleSuccess
-// );
-// export default router;
-
-
 import express from "express";
 import passport from "passport";
 import { googleSuccess, Login, Signup } from "../controllers/controller.js";
